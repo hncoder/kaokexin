4fff56f24376cc423e63f6873e56f0aaef274d54
hncoder <i@hncoder.com>
2018-04-04 18:20:58 +0800

    工程准备

---
+++ kaoke/src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
